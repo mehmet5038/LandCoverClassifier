b7a7eb2305fea331497cd28db58378a637df1d59
Mehmet Karatekin <krtknmm38@zoho.com>
2025-07-30 03:34:15 +0300

    feat(ml-features): training ml features

---
+++ utils/ml_features/grid_search.py
@@ -0,0 +1,50 @@
+from sklearn.model_selection import cross_val_score
+from itertools import product
+from tqdm import tqdm
+import time
+import numpy as np
+
+def grid_search(model_class, param_grid, X, y, cv=5, scoring="accuracy"):
+    best_score = -np.inf
+    best_model = None
+    best_params = None
+
+    keys = list(param_grid.keys())
+    values = list(param_grid.values())
+    all_combos = list(product(*values))
+
+    print("Denenecek hiperparametre kombinasyon sayısı: ", len(all_combos))
+
+    for combo in tqdm(all_combos, desc=f"{model_class.__class__.__name__} Grid Search"):
+        params = dict(zip(keys, combo))
+        model = model_class.set_params(**params)
+
+        start = time.time()
+        scores = cross_val_score(model, X, y, cv=cv, scoring=scoring, n_jobs=-1)
+        duration = time.time() - start
+        formatted_time = format_duration(duration)
+
+        avg_score = np.mean(scores)
+        print("Parametreler: ", params, " | Skor: ", avg_score, " | Süre: ", formatted_time)
+
+        if avg_score > best_score:
+            best_score = avg_score
+            best_model = model
+            best_params = params
+
+    best_model.fit(X, y)
+    return best_model, best_score, best_params
+
+def format_duration(seconds):
+    if seconds < 60:
+        secs = int(seconds // 1)
+        return str(secs) + " sn"
+    elif seconds < 3600:
+        minutes = int(seconds // 60)
+        secs = int(seconds % 60)
+        return str(minutes) + " dk" + str(secs) + " sn"
+    else:
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        secs = int(seconds % 60)
+        return str(hours) + " sa" + str(minutes) + " dk" + str(secs) + " sn"
---
+++ utils/ml_features/preprocesing.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+from skimage.feature import local_binary_pattern, hog
+from skimage.feature import graycomatrix, graycoprops
+from sklearn.decomposition import PCA
+
+
+def extract_features(image):
+    image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    # HOG
+    hog_feat = hog(image_gray, pixels_per_cell=(16, 16), cells_per_block=(2, 2), feature_vector=True)
+
+    # LBP
+    lbp = local_binary_pattern(image_gray, P=8, R=1, method='uniform')
+    lbp_hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, 11), density=True)
+
+    # HSV histogram
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hist_h = cv2.calcHist([hsv], [0], None, [8], [0, 180]).flatten()
+    hist_s = cv2.calcHist([hsv], [1], None, [8], [0, 256]).flatten()
+    hist_v = cv2.calcHist([hsv], [2], None, [8], [0, 256]).flatten()
+    hsv_hist = np.concatenate([hist_h, hist_s, hist_v])
+
+    # GLCM
+    glcm = graycomatrix(image_gray, [1], [0], levels=256, symmetric=True, normed=True)
+    glcm_feat = [graycoprops(glcm, prop).flatten()[0] for prop in ('contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation')]
+
+    return np.concatenate([hog_feat, lbp_hist, hsv_hist, glcm_feat])
+
+
+def apply_pca(features, n_components=100):
+    pca = PCA(n_components=n_components)
+    reduced_features = pca.fit_transform(features)
+    return reduced_features
